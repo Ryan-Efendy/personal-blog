84723b41bee8d634ce00203f1796212c313150a6
Ryan-Efendy <ryanefendy95@gmail.com>
2020-12-26 18:50:34 -0800

    update pro pic css

--- src/components/Sidebar/Author/Author.js
+++ src/components/Sidebar/Author/Author.js
@@ -18,8 +18,8 @@ const Author = ({ author, isIndex }: Props) => (
       <img
         src={withPrefix(author.photo)}
         className={styles['author__photo']}
-        width="300"
-        height="200"
+        width="auto"
+        // height="200"
         alt={author.name}
       />
     </Link>
